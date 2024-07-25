5f0bc5bb49db732fd98a3edf25fcd46bc33dd0da
sajjathossain <sajjathossain.official@gmail.com>
2024-07-25 20:08:17 +0600

    chore(color): add opacity

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,8 +1,10 @@
 export const generateRandomColor = () => {
-  let letters = '0123456789ABCDEF';
+  const letters = '0123456789ABCDEF';
   let color = '#';
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
+
+  color += '30';
   return color as `#${string}`;
 };
